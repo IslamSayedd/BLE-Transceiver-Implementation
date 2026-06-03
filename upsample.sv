e64dc714d01e52db2d578b059d7ea411410b2aa2
JuliaREmil <juliaramiemilamir@gmail.com>
2026-06-03 14:40:14 +0300

    Update upsample.sv

--- upsample.sv
+++ upsample.sv
@@ -11,14 +11,19 @@ module upsample #
   output reg bit_upsample_o,
   output reg bit_upsample_valid_o
 );
+
 integer counter;
 integer counter_out;
 integer loop_out;
-reg phy_bit_reg_in;
-reg [ SAMPLE_PER_SYMBOL -1 : 0] bit_upsample_reg [DATA_WIDTH - 1 : 0];
-logic out_flag;
+integer k;
+reg [SAMPLE_PER_SYMBOL-1:0] bit_upsample_reg [DATA_WIDTH-1:0];
+wire out_flag;
 
-integer k; // for reset loop
+wire [SAMPLE_PER_SYMBOL-1:0] live_sample;
+assign live_sample = {SAMPLE_PER_SYMBOL{!NRZ_i[1]}};
+
+wire [SAMPLE_PER_SYMBOL-1:0] current_read;
+assign current_read = (counter_out == counter) ? live_sample : bit_upsample_reg[counter_out];
 
 always @ (posedge clk or negedge rst_n) 
   begin
@@ -27,7 +32,6 @@ always @ (posedge clk or negedge rst_n)
         bit_upsample_o       <= 'b0;
         bit_upsample_valid_o <= 'b0;
         counter              <= 'b0;
-        phy_bit_reg_in       <= 'b0;
         loop_out             <= 'b0;
         counter_out          <= 'b0;
         for (k = 0; k < DATA_WIDTH; k = k + 1)
@@ -40,45 +44,47 @@ always @ (posedge clk or negedge rst_n)
           begin
             if (counter == DATA_WIDTH)
               counter <= 'b0;
-            else begin
-              bit_upsample_reg[counter] <= {SAMPLE_PER_SYMBOL{!NRZ_i[1]}};
-              counter <= counter + 'b1;
-            end
+            else
+              begin
+                bit_upsample_reg[counter] <= live_sample;
+                counter <= counter + 'b1;
+              end
           end
-        else begin
-          counter <= 'b0;
-          bit_upsample_reg[counter] <= 'b0;
-        end
+        else
+          begin
+            counter <= 'b0;
+          end
 
         // Output side
         if (out_flag)
           begin
             if (counter_out != DATA_WIDTH)
               begin
-                if (loop_out != SAMPLE_PER_SYMBOL - 1)
+                bit_upsample_o       <= current_read[loop_out];
+                bit_upsample_valid_o <= 'b1;
+                if (loop_out == SAMPLE_PER_SYMBOL - 1)
                   begin
-                    bit_upsample_o       <= bit_upsample_reg[counter_out][loop_out];
-                    loop_out             <= loop_out + 'b1;
-                    bit_upsample_valid_o <= 'b1;
+                    loop_out    <= 'b0;
+                    counter_out <= counter_out + 1;
                   end
                 else
                   begin
-                    bit_upsample_o       <= 'b0;    
-                    loop_out             <= 'b0;
-                    counter_out          <= counter_out + 1;
-                    bit_upsample_valid_o <= 'b1;
+                    loop_out <= loop_out + 'b1;
                   end
               end
           end
         else
           begin
             counter_out          <= 'b0;
+            loop_out             <= 'b0;
             bit_upsample_o       <= 'b0;           
             bit_upsample_valid_o <= 'b0;
           end
       end
   end
 
-assign out_flag = ((NRZ_valid_i) || counter_out != 'b0) && !(counter_out == DATA_WIDTH && loop_out == 1) ? 'b1 : 'b0;
+assign out_flag = (NRZ_valid_i && counter == 'b0) || 
+                  (counter_out == 'b0 && loop_out != 'b0) || 
+                  (counter_out != 'b0 && counter_out != DATA_WIDTH) ? 'b1 : 'b0;
 
 endmodule
